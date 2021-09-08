de1943b52e27b6db9b6a5f9be1c49537a8ee72a5
gualtierigio <gualtierigio@gmail.com>
2021-09-08 16:23:39 +0200

    card complete

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,23 +2,55 @@
 let card = {
     name : "Tarmogoyf",
     manaCost = {
-        costoNeutro : 1,
+        common : 1,
         forest : 1,
         plains : 0,
         island : 0,
         swamp : 0,
         mountain : 0,
     },
+
+    function(){
+        let sum = 0;
+        for (let type in this.manaCost){
+            sum += this.manaCost[type];
+        }
+        return sum;
+    },
+
     tipeOfCard : "creature", 
     tipeOfCreature : "lhurgoyf",
+
     edition = {
         rarity : "ultra-rare",
-        yearEdition : "2014",
+        yearEdition : "2017",
         existentCards : 249,
     },
+
     flavourText : {
         author : "Rean Barger",
         descriptiopn : "What doesn't grow, dies. And what dies grows the tarmogoyf."
-    }
+    },
 
+    abilities : [
+        {manaCostAbilities = {
+            common : 0,
+            forest : 0,
+            plains : 0,
+            island : 0,
+            swamp : 0,
+            mountain : 0,
+        },
+        description : "Tarmogoyf's power is equal to the number of card types among cards in all graveyards and its toughness is equal to that number plus 1.",
+        tap : false,
+    }],
+
+    cardOfTheExistentCards : 141,
+
+    costitution : 1,
+    strength : 0,
+
+    borderCard : "black",
+
+    cardImg : "..img/tarmogoyf.png"
 }
