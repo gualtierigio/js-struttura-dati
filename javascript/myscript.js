a083994de7e02da4b3572b5b4c1cf9c17bb1363a
gualtierigio <gualtierigio@gmail.com>
2021-09-08 15:55:55 +0200

    aggiungo dati all oggetto

---
+++ javascript/myscript.js
@@ -0,0 +1,24 @@
+
+let card = {
+    name : "Tarmogoyf",
+    manaCost = {
+        costoNeutro : 1,
+        forest : 1,
+        plains : 0,
+        island : 0,
+        swamp : 0,
+        mountain : 0,
+    },
+    tipeOfCard : "creature", 
+    tipeOfCreature : "lhurgoyf",
+    edition = {
+        rarity : "ultra-rare",
+        yearEdition : "2014",
+        existentCards : 249,
+    },
+    flavourText : {
+        author : "Rean Barger",
+        descriptiopn : "What doesn't grow, dies. And what dies grows the tarmogoyf."
+    }
+
+}
